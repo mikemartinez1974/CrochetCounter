b9d682c4fbce3c4888285f24a4c58fefbd230efc
mikemartinez1974 <mike.d.martinez@gmail.com>
2016-01-28 23:51:22 -0600

    Added, updated, and included color.js so I could use it for validation and/or error checking.
    Coded a new, more robust, version of the counterbox object and made it object-oriental.
    
    Included some basic styling for progress bars and the counterbox container.

--- app/scripts/CounterBoxes.js
+++ app/scripts/CounterBoxes.js
@@ -15,7 +15,7 @@ var counterBoxFactory = {
     this.buttons = [];
     $container.empty();
 
-    //create a new collection of buttons = boxcount param.
+    //create a new collection of buttons === boxcount param.
     for(var i = 1; i<=boxCount; i++){
       this.buttons[i] = counterBox(i);
     }
@@ -28,14 +28,14 @@ var counterBoxFactory = {
       $("#box" + counterbox.index).on("click", counterbox, function() {
         var $image = $("#image" + counterbox.index);
         var css = $image.css("display");
-        var action = null;
+        //var action = null;
         if(css === "none") {
           $image.css({display:"block"});
-          action = "clicked"
+          //action = "clicked"
         }
         else {
           $image.css({display:"none"});
-          action = "unclicked";
+          //action = "unclicked";
         }
 
         counterBoxFactory.buttons.forEach(function(button){
@@ -50,14 +50,12 @@ var counterBoxFactory = {
         });
 
         pb.animate(counterbox.index / counterBoxFactory.buttons.length);
-
       })
     });
   }
 };
 
 function counterBox(labelText, forecolor, backcolor) {
-  var imageFile = "transCheck.png";
   var fcolor = forecolor || "black";
   var bcolor = backcolor || "white";
 
@@ -87,4 +85,164 @@ function createCounterButtons(quantity) {
   }
 }
 
+function CounterButton() {
+  var _container = null;
+  Object.defineProperty(this, "container", {
+    get:  function() {
+            return _container;
+          },
+    set:  function(val) {
+            if(val instanceof jQuery) {
+              _container = val;
+            } else {
+              throw new InvalidArgumentException("CounterButton.container", "jquery", "val", val);
+            }
+          }
+  });
 
+  var _index = null;
+  Object.defineProperty(this, "index",{
+    get:  function() {
+            return _index;
+          },
+    set:  function(val) {
+            if(isNumeric(val)) {
+              _index = parseInt(val.toString());
+            } else {
+              throw new InvalidArgumentException("CounterButton.index", "number", "val", val);
+            }
+          }
+  });
+
+  var _imageid = null;
+  Object.defineProperty(this, "imageId", {
+    get:  function() {
+            if(_imageid === null) {
+              _imageid = "image" + this.index;
+            }
+            return _imageid;
+          }
+  });
+
+  var _imagesource = "images/transCheck.png";
+  Object.defineProperty(this, "imageSource", {
+    get:  function() {
+            return _imagesource;
+          },
+    set:  function(val) {
+            if(val == false) {throw new InvalidArgumentException("CounterBox.imageSource","url or path","val",val);}
+            _imagesource = val;
+          }
+  });
+
+  var _image = null;
+  Object.defineProperty(this, "image", {
+    get:  function() {
+            if(!_image) {
+              _image = $("#" + this.imageId);
+            }
+          }
+  });
+
+  var _bgcolor = "white";
+  Object.defineProperty(this, "bgColor", {
+    get:  function() {
+            return _bgcolor;
+          },
+    set:  function(val) {
+            if($c.isColorValid(val))
+            {
+              _bgcolor = val;
+            } else {
+              throw new InvalidArgumentException("CounterBox.bgColor","color", "val", val);
+            }
+          }
+  });
+
+  var _fcolor = "black";
+  Object.defineProperty(this, "foreColor", {
+    get:  function() {
+            return _fcolor;
+          },
+    set:  function(val) {
+            if($c.isColorValid(val)) {
+              _fcolor = val;
+            } else {
+              throw new InvalidArgumentException("CounterBox.foreColor", "color", "val", val);
+            }
+          }
+  });
+
+  var _checked = false;
+  Object.defineProperty(this, "checked", {
+    get:  function () {
+      return _checked;
+    },
+    set:  function (val) {
+            var acceptableValues = ["checked","1","true","unchecked","0","false"];
+            switch(acceptableValues.indexOf(val.toString().toLowerCase())) {
+              case 0:
+              case 1:
+              case 2:
+                    _checked = true;
+                    break;
+              case 3:
+              case 4:
+              case 5:
+                    _checked = false;
+                    break;
+              default:
+                    throw new InvalidArgumentException("CounterBox.checked","one of " + acceptableValues.toString(), "val", val);
+            }
+            this.image.css({display: _checked ? "block" : "none" });
+          }
+  });
+
+  Object.defineProperty(this, "html", {
+    get:  function () {
+
+            var labelDivStyle = "style='position:relative; width:100%; height:100%; text-align: center'";
+            var imageDivStyle = "style='position:relative; top:-30px; left:0; z-index:1; display:none;'";
+            var imageStyle = "style='height:25px; width:30px; margin: auto;'";
+            var boxStyle = "style='float:left; color:" + this.foreColor + "; background-color: " + this.bgColor + "; height: 25px; width: 30px; margin-top:2px; margin-left:2px; border: solid black 1px; padding-top:5px;'";
+
+            var imageMarkup = "<img id='_" + this.imageId + "' src='" + this.imageSource + "' " + imageStyle + ">";
+            var labelDiv = "<div id='label" + this.text + "' " + labelDivStyle + " >" + this.text + "</div>";
+            var imageDiv = "<div id='" + this.imageId + "' " + imageDivStyle + " >" + imageMarkup + "</div>";
+            var counterBoxMarkup = "<div id='box"   + this.text + "' " + boxStyle + ">" + labelDiv + imageDiv + "</div>";
+
+            return counterBoxMarkup;
+          }
+  });
+
+  var _selector = null;
+  Object.defineProperty(this, "selector", {
+    get:  function () {
+            if(!(_selector instanceof jQuery)) {
+              _selector = $("#box" + this.id);
+            }
+
+            return _selector;
+          }
+  });
+
+  this.render = function () {
+    this.container.append(this.html);
+    this.selector.on("click", this, CounterButton_Click);
+  };
+
+  function CounterBoxes_Click(e, sender) {
+    this.checked = !this.checked;
+  }
+
+}
+
+
+//Exceptions
+function InvalidArgumentException(procedure, type, param, value) {
+  this.message = "Invalid Argument Exception @ " + procedure + ".  " + param + " requires " + type + "(" + value + ")";
+}
+
+function NotImplementedException(procedure) {
+  this.message = "Not Implemented Exception @ " + procedure;
+}
